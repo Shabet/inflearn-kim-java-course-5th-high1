11f5f5dd3b1197d7c6bbf53b7bb18ca6184e4587
Shabet <pakjunyoung@nate.com>
2024-08-14 09:32:19 +0900

    section07 내용 추가

---
+++ src/section07/thread/sync/BankAccountV4.java
@@ -0,0 +1,59 @@
+package section07.thread.sync;
+
+import section06.thread.sync.BankAccount;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+    private int balanace;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV4(int initialBalance) {
+        this.balanace = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        lock.lock(); // ReentrantLock 이용하여 lock 걸기
+        try {
+            log("[검증 시작] 출금액: " + amount + " , 잔액: " + balanace);
+
+            // 잔고가 출금액 보다 적으면, 진행하면 안됨
+            if (balanace < amount) {
+                log("[검증 실패] 출금액: " + amount + " , 잔액: " + balanace);
+                return false;
+            }
+
+            // 잔고가 출금액 보다 많으면, 진행
+            log("[검증 완료] 출금액: " + amount + " , 잔액: " + balanace);
+            sleep(1000); // 출금에 걸리는 시간으로 가정
+            balanace -= amount;
+
+            log("[출금 완료] 출금액: " + amount + " , 잔액: " + balanace);
+        }
+        finally {
+            lock.unlock(); // ReentrantLock 이용하여 lock 해제(반드시 호출해야함)
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock(); // ReentrantLock 이용하여 lock 걸기
+        try {
+            return balanace;
+        }
+        finally {
+            lock.unlock(); // ReentrantLock 이용하여 lock 해제(반드시 호출해야함)
+        }
+    }
+}
---
+++ src/section07/thread/sync/lock/LockSupportMainV2.java
@@ -0,0 +1,29 @@
+package section07.thread.sync.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV2 {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTest(), "Thread-1");
+        thread1.start();
+
+        // 잠시 대기하여 Thread-1 이 park 상태에 빠질 시간을 준다.
+        sleep(100);
+        log("Thread-1 statre: " + thread1.getState());
+    }
+
+    static class ParkTest implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작");
+            LockSupport.parkNanos(2_000_000_000);
+            log("park 종료, status: " + Thread.currentThread().getState());
+            log("인터럽트 상태: " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
