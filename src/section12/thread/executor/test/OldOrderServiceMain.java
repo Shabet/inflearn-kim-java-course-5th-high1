26e140e6365b512faca76b9e6dcd736726d2b97a
Shabet <pakjunyoung@nate.com>
2024-08-16 17:15:34 +0900

    section12 내용 추가

---
+++ src/section12/thread/executor/test/OldOrderServiceMain.java
@@ -0,0 +1,10 @@
+package section12.thread.executor.test;
+
+public class OldOrderServiceMain {
+
+    public static void main(String[] args) {
+        String orderNo = "Order#1234"; // 예시 주문 번호
+        OldOrderService orderService = new OldOrderService();
+        orderService.order(orderNo);
+    }
+}
